b0d9cc01d8d0c4949850d9c12b5bbc7a39ee2535
yormangomez <yosmangomez@hotmail.com>
2024-11-25 18:11:17 -0500

    update

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,13 +2,9 @@
 
 import 'dart:async';
 import 'dart:developer';
-// import 'dart:io' as io;
 import 'package:api_helper/api_helper.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pragma_test/injection/injection_container.dart' as injection;
-// import 'package:dynatrace_flutter_plugin/dynatrace_flutter_plugin.dart'
-//     as dynatrace;
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -45,11 +41,6 @@ class AppBlocObserver extends BlocObserver {
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
     LoggerEvents().error('onError(${bloc.runtimeType}, $error, $stackTrace)');
-    if (!kIsWeb) {
-      FirebaseCrashlytics.instance.recordError(error, stackTrace, fatal: true);
-      // final platform = io.Platform.isAndroid ? dynatrace.Platform.Android : dynatrace.Platform.iOS;
-      // dynatrace.Dynatrace().reportZoneStacktrace(error, stackTrace, platform: platform);
-    }
   }
 }
 
@@ -67,52 +58,22 @@ Future<void> bootstrap({
 }) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
-    if (!kIsWeb) {
-      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
-      if (details.stack != null) {
-        // final platform = io.Platform.isAndroid ? dynatrace.Platform.Android : dynatrace.Platform.iOS;
-        // dynatrace.Dynatrace().reportZoneStacktrace(details, details.stack!, platform: platform);
-      } else {
-        /// Define error code to [1000]
-        // dynatrace.Dynatrace().reportError(details.exceptionAsString(), 1000);
-      }
-    }
   };
 
   // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
   PlatformDispatcher.instance.onError = (error, stack) {
     log(error.toString(), stackTrace: stack);
-    if (!kIsWeb) {
-      // FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      // final platform = io.Platform.isAndroid ? dynatrace.Platform.Android : dynatrace.Platform.iOS;
-      // dynatrace.Dynatrace().reportZoneStacktrace(error, stack, platform: platform);
-    }
     return true;
   };
 
   if (kIsWeb && const bool.hasEnvironment('DYNATRACE_WEB_ID')) {
-  } else if (const bool.hasEnvironment('DYNATRACE_APP_ID') && const bool.hasEnvironment('DYNATRACE_BEACON_URL')) {
-    // Add cross-flavor configuration here
-    // await dynatrace.Dynatrace().startWithoutWidget(
-    //   configuration: const dynatrace.Configuration(
-    //     applicationId: String.fromEnvironment('DYNATRACE_APP_ID'),
-    //     beaconUrl: String.fromEnvironment('DYNATRACE_BEACON_URL'),
-    //     userOptIn: true,
-    //   ),
-    // );
-  }
+  } else if (const bool.hasEnvironment('DYNATRACE_APP_ID') && const bool.hasEnvironment('DYNATRACE_BEACON_URL')) {}
 
   Bloc.observer = const AppBlocObserver();
 
   await injection.init(
     environment: env,
-    onError: (exception, stack) {
-      if (!kIsWeb) {
-        FirebaseCrashlytics.instance.recordError(exception, stack);
-        // final platform = io.Platform.isAndroid ? dynatrace.Platform.Android : dynatrace.Platform.iOS;
-        // dynatrace.Dynatrace().reportZoneStacktrace(exception, stack ?? StackTrace.fromString(''), platform: platform);
-      }
-    },
+    onError: (exception, stack) {},
   );
   runApp(await builder());
 }
